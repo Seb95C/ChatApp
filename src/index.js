e0b97869882fa48b5c21c824f2e14b70429e9283
Sebastian Calcan <sebastian95calcan@gmail.com>
2023-01-19 10:21:39 +0200

    Integrated Web Socket

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 // Node core modules
 const path = require('path')
+const http = require('http')
+
+// NPM modules
+const express = require('express')
+const socketio = require('socket.io')
 
 // Path management
 const staticDirPath = path.join(__dirname, '../public')
 
-// Setting up Express server
-const express = require('express')
+// Setting up server
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+// Set serer port
 const port = process.env.PORT || 3000
 
 // Configure static content
 app.use(express.static(staticDirPath))
 
+io.on('connection', () => {
+    console.log('New WebSocket connection')
+})
+
 // Server start-up
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server up on port ${port}`)
 })
